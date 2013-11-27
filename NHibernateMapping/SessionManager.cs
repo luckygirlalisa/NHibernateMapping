70115e8135ec9f59cc4a007781fcfdd1086b1469
zfxiao <zfxiao@thoughtworks.com>
2013-11-27 09:20:03 +0800

    add criteria query methods

--- NHibernateMapping/SessionManager.cs
+++ NHibernateMapping/SessionManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Domain.Entities;
-using NHibernate; 
+using NHibernate;
+using NHibernate.Criterion;
 
 namespace NHibernateMapping
 {
@@ -11,7 +12,7 @@ namespace NHibernateMapping
         {
             this.session = session;
         }
-
+        #region get data from DB
         public IList<Customer> GetAllCustomer()
         {
             return session.CreateQuery("from Customer").List<Customer>();
@@ -35,5 +36,17 @@ namespace NHibernateMapping
                           .SetParameter("lastName", lastName)
                           .List<Customer>();
         }
+
+        public IList<Customer> GetCustomerByLastNameWithCriteria(string lastName)
+        {
+            return session.CreateCriteria<Customer>().Add(Restrictions.Eq("LastName", lastName)).List<Customer>();
+        }
+
+        public IList<Customer> GetCustomerBySample(Customer customerSample)
+        {
+            Example example = Example.Create(customerSample).IgnoreCase();
+            return session.CreateCriteria<Customer>().Add(example).List<Customer>();
+        }
+#endregion
     }
 }
--- Test/GetDataFromDBTest.cs
+++ Test/GetDataFromDBTest.cs
@@ -51,39 +51,24 @@ namespace MappingTest
 
             PrintCustomerList(allCustomerWithLastNameXiao);
         }
-//
-//        [Test]
-//        public void should_get_customers_in_customer_table_using_criteria()
-//        {
-//            var allCustomWithLastNameXiao = sessionManager.GetCustomerByLastNameWithCriteria("Xiao");
-//
-//            PrintCustomerList(allCustomWithLastNameXiao);
-//        }
-//
-//        [Test]
-//        public void should_get_customers_in_customer_table_using_criteria_between()
-//        {
-//            var allCustomWithLastNameXiao = sessionManager.GetCustomerByIdRuleWithCriteria();
-//
-//            PrintCustomerList(allCustomWithLastNameXiao);
-//        }
-//
-//        [Test]
-//        public void should_get_customer_with_same_properties_with_sample_using_criteria()
-//        {
-//            var customerSample = new Customer { CustomerId = 10, FirstName = "Zhanfei", LastName = "Xiao" };
-//
-//            var customer = sessionManager.GetCustomerBySample(customerSample);
-//
-//            PrintCustomerList(customer);
-//
-//            //            Assert.That(customer.FirstName, Is.EqualTo("Zhanfei"));
-//            //            Assert.That(customer.LastName, Is.EqualTo("Xiao"));
-//            //            Assert.That(customer.Id, Is.EqualTo(10));
-//
-//            //            Console.WriteLine(new {customer.FirstName, customer.LastName, customer.Id});
-//        }
-//
+
+        [Test]
+        public void should_get_customers_in_customer_table_using_criteria()
+        {
+            var allCustomWithLastNameXiao = sessionManager.GetCustomerByLastNameWithCriteria("Xiao");
+
+            PrintCustomerList(allCustomWithLastNameXiao);
+        }
+
+        [Test]
+        public void should_get_customer_with_same_properties_with_sample_using_criteria()
+        {
+            var customerSample = new Customer {CustomerId = 10, FirstName = "Zhanfei", LastName = "Xiao"};
+
+            var customer = sessionManager.GetCustomerBySample(customerSample);
+
+            PrintCustomerList(customer);
+        }
 
         private static void PrintCustomerList(IList<Customer> customers)
         {
